f376a22da0ec73ea5cc8df6e3ebdb88f79c91b3d
JJTech0130 <53275876+JJTech0130@users.noreply.github.com>
2021-12-31 13:03:53 -0500

    chore!: modify test to use new book type

--- bibliotheca_test.go
+++ bibliotheca_test.go
@@ -8,7 +8,7 @@ import (
 
 func Test(t *testing.T) {
 	baseURL, _ := url.Parse("https://ebook.yourcloudlibrary.com/uisvc/BethlehemDistrictLibraries")
-	
+
 	bookId := "ammqdg9"
 
 	session, err := Login("11111", baseURL)
@@ -17,35 +17,30 @@ func Test(t *testing.T) {
 		log.Fatal(err)
 	}
 
-	item, err := GetItem(bookId, s)
+	b, err := NewBook(bookId, s)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	borrowed, err := Borrowed(s)
-	log.Println(borrowed)
-	test, _ := Download(bookId, s)
-	log.Println(test)
+	log.Println("Book: " + b.Title)
+	log.Println("Author: " + b.Author)
+	log.Println("ISBN: " + b.ISBN)
+	log.Println("Action: " + b.Action)
 
-	//log.Println(item)
-	log.Println("Book: " + item["Title"].(string))
-	log.Println("ISBN: " + item["ISBN"].(string))
-	action := item["AllowedPatronAction"].(string)
-	log.Println("Action: " + action)
-
-	if action == "Return" {
-		log.Println("Returning book...")
-		/*err = Return(bookId, s)
+	if b.Action == Returnable {
+		log.Println("Downloading book...")
+		ascm, err := Download(bookId, s)
 		if err != nil {
 			log.Fatal(err)
-		}*/
-	} else if action == "Borrow" {
+		}
+		log.Println(ascm)
+	} else if b.Action == Borrowable {
 		log.Println("Borrowing book...")
 		err = Borrow(bookId, s)
 		if err != nil {
 			log.Fatal(err)
 		}
 	} else {
-		log.Fatal("Could not borrow or return book")
+		log.Fatal("Book is in unknown state")
 	}
 }
